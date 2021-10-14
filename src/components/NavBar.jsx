1074b1f101ae01b568a68b86123410328b3fa4f2
Macu Ruiz <ruiz.martinalara@gmail.com>
2021-10-13 22:16:46 -0300

    Working on the Navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,32 @@
 
+import { Navbar, Container, NavDropdown, Nav } from 'react-bootstrap';
+import styles from '../styles.css'
+
 const NavBar = () => {
     return(
-        <div>
-            Hola soy un menu
-        </div>
+        <Navbar id="NavBar" expand="lg">
+            <Container>
+                <a>
+                    <img src="../../logo2.png" alt="" />
+                </a>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link href="#home">Conocenos</Nav.Link>
+                    <Nav.Link href="#link">¿Cómo comprar?</Nav.Link>
+                    <NavDropdown title="Categorías" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Fantasía</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">Romance</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">ciencia ficción</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">Policiales</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/3.4">No Ficción</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+
     )
 }
 
